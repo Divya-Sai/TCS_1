bc1e83e071009878a05e512a5439d7715ed5b235
Divya Bugatha <divya.santhoshi2000@gmail.com>
2020-12-24 11:16:14 +0000

    First Commit for TCS_1 project

---
+++ src/main/java/pages/CrossBrowsingTest.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CrossBrowsingTest {
+	
+	WebDriver driver ;
+	public void browser(String BrowserName) {
+		
+		if(BrowserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver",".\\chromedriver.exe");
+			driver =new ChromeDriver();
+			
+		}
+		else if (BrowserName.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", ".\\geckodriver.exe");
+			driver =new FirefoxDriver();
+			
+		}
+	}
+
+}
